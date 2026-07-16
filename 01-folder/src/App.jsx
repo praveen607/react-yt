2c42556bd04d2d566f872e47add72d7429f917e8
praveen <praveendasari155@gmail.com>
2026-07-16 19:52:13 +0530

    Initial commit

---
+++ 01-folder/src/App.jsx
@@ -0,0 +1,18 @@
+import React from 'react'
+
+const App = () => {
+  return (
+    // <> </> this is called fragments, you can return two or more divs with help of fragments(<></>)
+    <>
+    <div>
+      <h1>This is ReactJS</h1>
+      <h3>hello</h3>
+    </div>
+    <div>
+      <h2>this is another div using fragments</h2>
+    </div>
+    </>
+  )
+}
+
+export default App
